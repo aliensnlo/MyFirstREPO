6fa8af1d8e2702347c44b0932984fe8b1880121a
? <ponyaal@yandex.ru>
2023-08-15 14:25:33 +0300

    js+

---
+++ JS/1/script.js
@@ -0,0 +1,49 @@
+const myName = "Ruslan";
+const myAge = 24;
+const yuriIsGey = false;
+const q = "q";
+const w = 30;
+const prikol = true;
+const i = "i";
+const p = 20;
+const pol = false;
+const head = "Голова";
+const body = "Тело";
+const age = 20;
+const bodyage = 31;
+const ok = false;
+const k = "kasha";
+const l = "lopatoi";
+const z = "zero";
+const x = "xleb";
+const c = 1;
+const v = 2;
+const b = 3;
+const n = 4;
+const m = true;
+const s = false;
+const d = true;
+const f = false;
+const r = "Чебурашкой";
+const lol = "smex";
+const god = 25;
+const e = true;
+console.log(
+  `привет меня зовут ${myName} и мне ${myAge} мой учитель ${
+    yuriIsGey ? "гей" : "не гей"
+  }`
+);
+
+console.log(`q ${q} w ${w} прикол ${prikol}`);
+console.log(`i ${i} p ${p} получилось ${pol ? "да" : "нет"}`);
+
+console.log(
+  `Моя ${head} выше ${body} голове ${age} а телу ${bodyage} дА ${
+    ok ? "Да" : "Net"
+  }`
+);
+console.log(
+  `Люблю кушать ${k} и работать ${l}, после умножать все на ${z} и заливать в себя ${x}. Хочу зарабатывать ${c} ${v} ${b} ${n}, и тогда правда будет за ${r}, вот это ${lol} в ${god} год ${
+    m ? "Было" : "Не было"
+  }`
+);
